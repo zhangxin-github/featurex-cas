1e166c13f8478ad9ba1f3df507348eb45b9ec83a
xinzhang <zhangxin@yunjiacloud.com>
2019-01-09 21:22:21 +0800

    Initial commit

---
+++ featurex-cas-webapp-support/src/main/java/org/wizard/featurex/cas/FeatureXCasApplication.java
@@ -0,0 +1,12 @@
+package org.wizard.featurex.cas;
+
+import org.springframework.boot.SpringApplication;
+import org.wizard.web.WizardBootApplication;
+
+@WizardBootApplication
+public class FeatureXCasApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FeatureXCasApplication.class, args);
+    }
+
+}
